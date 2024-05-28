f3245fedd29881247087074a8ec3ec8d9e24f50c
HenryToti <henryokoth3@gmail.com>
2024-05-28 10:52:50 +0300

    Initial Commit

---
+++ Authenticator/AuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorApp.swift
+//  Authenticator
+//
+//  Created by Billy Okoth on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
